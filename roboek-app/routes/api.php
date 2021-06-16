8e3e1ea4ecfcae7046ab35013797301ed5729b10
TimovandenBerg <mrtimogames@gmail.com>
2021-06-16 15:43:45 +0200

    Je kan nu de namen in de database schieten.

--- roboek-app/routes/api.php
+++ roboek-app/routes/api.php
@@ -18,6 +18,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
+
 //ROUTES VOOR VIRTUELE BIBLIOTHEEK:
 Route::get('/bibliotheek', [\App\Http\Controllers\BibliotheekController::class, 'index']);
 Route::get('/bibliotheek/details/{id}', [\App\Http\Controllers\BibliotheekController::class, 'show']);
@@ -25,3 +27,5 @@ Route::get('/bibliotheek/{genre}', [\App\Http\Controllers\BibliotheekController:
 Route::get('/bibliotheek/choose/genre', [\App\Http\Controllers\BibliotheekController::class, 'chooseGenre']);
 Route::post('/bibliotheek/favorite/{id}', [\App\Http\Controllers\BibliotheekController::class, 'addToFavorites']);
 Route::delete('/bibliotheek/favorite/{id}', [\App\Http\Controllers\BibliotheekController::class, 'deleteFromFavorites']);
+
+Route::post('/user/create', [\App\Http\Controllers\UserController::class, 'store']);
