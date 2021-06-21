a9396c44c9eb9f03fa7c292be6cf5b286b504223
Daniel van Droffelaar <daniel@realafford.nl>
2021-06-21 13:20:40 +0200

    Heb medaille toegevoegd aan uitdaging

--- roboek-app/database/seeders/UitdagingenTableSeeder.php
+++ roboek-app/database/seeders/UitdagingenTableSeeder.php
@@ -20,6 +20,7 @@ class UitdagingenTableSeeder extends Seeder
             'vraag' => "0000000000000leon0000weerwolf0d00000u000o0000bril0l00000a000f0rood0m00j000000a00etimmiea000000000n00",
             'antwoorden' => "weerwolf maan dolfje timmie noura leo rood bril" ,
             'beloning' => 100,
+            'medaille_id' => 5
         ]);
     }
 }
