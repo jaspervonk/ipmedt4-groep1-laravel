aa128d73740f80ff831c46debedcf0cb9feee33f
s1115351 <s1115351@student.hsleiden.nl>
2021-06-22 20:31:17 +0200

    Verkeerde model gelinkt in het bestand zelf

--- roboek-app/app/Models/Puzzel.php
+++ roboek-app/app/Models/Puzzel.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class GekozenBoek extends Model
+class Puzzel extends Model
 {
     protected $table = "uitdagingen";
 }
